9ecbf74df957ee9488e35261a2fe62567c3802fb
RTM <rtmdoes.work@gmail.com>
2026-04-09 23:16:37 +0600

    del

--- Data Structures & Algorithms/is-anagram/submission-18.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        s_words = {}
-        t_words = {}
-
-        s = s
-        t = t
-
-        return sorted(s) == sorted(t)
